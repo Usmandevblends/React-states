33bd1ffcbbdf283475e609405e41bf1b67010073
Usmandevblends <167763547+Usmandevblends@users.noreply.github.com>
2024-09-04 16:00:20 +0500

    update package dependencies aur implement tailwind css

--- state/src/App.js
+++ state/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import { useState, useEffect } from "react";
+import "./output.css";
 function App() {
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
-    <div>App</div>
-  )
+    <div className="flex items-center justify-center flex-col ">
+      <p className="text-3xl">You clicked {count} times</p>
+      <button
+        onClick={() => setCount(count + 1)}
+        className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+        Click me
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
