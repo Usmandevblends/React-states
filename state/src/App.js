b79dcfcfdba6ef771f4833acb7e69fd3c4664a6a
Usmandevblends <167763547+Usmandevblends@users.noreply.github.com>
2024-09-05 16:44:12 +0500

    changes in app.css

--- state/src/App.js
+++ state/src/App.js
@@ -16,8 +16,8 @@ function App() {
   return (
     <>
       <div className="flex flex-col gap-5 items-center justify-center h-screen">
-        <h1 className="text-3xl">Count : {count} </h1>
-        <h1 className="text-3xl">Data : {data} </h1>
+        <h1 className="text-3xl font-bold">Count : {count} </h1>
+        <h1 className="text-3xl font-bold">Data : {data} </h1>
         <div className="flex gap-2">
           <button onClick={() => setCount(count + 1)} className="text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90  focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 me-2 mb-2">Update count</button>
           <button onClick={() => setData(data + 1)} className="text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90  focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 me-2 mb-2">Update data</button>
