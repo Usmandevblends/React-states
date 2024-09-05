9fa6e80d70d76a3afc9918ce37150e4ea14c05cb
Usmandevblends <167763547+Usmandevblends@users.noreply.github.com>
2024-09-05 12:15:04 +0500

    changes in app

--- state/src/App.js
+++ state/src/App.js
@@ -1,25 +1,27 @@
-import { useState, useEffect } from "react";
-import "./output.css";
+import React, { useEffect, useState } from "react";
 import "./style.css";
+import "./output.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [num, setNum] = useState(10000);
 
   useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  }, [count]);
+    // alert("I am clicked");
+  }, [num]);
 
   const handleOnClick = () => {
-    setCount(count + 1);
-  }
+    setNum(num + 1);
+  };
 
   return (
-    <div className="flex items-center justify-center flex-col mt-10">
-      <p className="text-3xl">You clicked {count} times</p>
-      <button onClick={handleOnClick}
-        className="text-white bg-gradient-to-r from-cyan-500 via-black-600 to-teal-700 hover:bg-gradient-to-br  focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-white-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-3">
-        Click me
-      </button>
+    <div className="container `">
+      <div className="flex flex-col items-center justify-center mt-10">
+        <button
+          className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          onClick={handleOnClick}>
+          Click me {num}
+        </button>
+      </div>
     </div>
   );
 }
