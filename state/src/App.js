a7f8fc11011a9e60ade6ac219fabe427618e6b9e
Usmandevblends <167763547+Usmandevblends@users.noreply.github.com>
2024-11-12 11:58:26 +0500

    changes in app

--- state/src/App.js
+++ state/src/App.js
@@ -1,15 +1,18 @@
-import React, { } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import "./output.css";
+
 function App() {
-
-  React.useEffect(() => {
-    alert("Hey Welcome to my page");
-  }, []);
-
+  const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
+  useEffect(() => {
+    setCalculation(count + 2)
+  }, [count])
   return (
     <>
-
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
     </>
   );
 }
