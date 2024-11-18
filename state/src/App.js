0ddf4f99d5e8808fb216b6a7c4a13d2e8e4d44ac
usman <imdevusman@gmail.com>
2024-11-18 16:57:19 +0500

    main

--- state/src/App.js
+++ state/src/App.js
@@ -1,29 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import "./style.css";
 import "./output.css";
 
 function App() {
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const fetchedData = await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve('Fetched Data');
-        }, 1000);
-      });
-      setData(fetchedData);
-    };
-  
-    fetchData();
-  }, [false]); 
+  const [data, setData] = useState('');
 
   return (
     <>
-      <div className='flex items-center justify-center gap-10 flex-col mt-5'>
-        <h1 className='text-3xl font-bold'>{data}</h1>
-        <button className='bg-lime-500 p-4 rounded-lg' onClick={() => setData('Usman')}>Update Data</button>
-      </div>
+      
     </>
   );
 }
